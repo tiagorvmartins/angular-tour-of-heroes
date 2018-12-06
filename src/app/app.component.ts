cc7412cc0eb9f79f4a80ed3d7784d010a1292156
Tiago Martins <tiagorvmartins@gmail.com>
2018-12-06 10:16:06 +0000

    Final version angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+    title = 'Tour of Heroes';
+
+    name(): string {
+        return this.title;
+    }
 }
+
